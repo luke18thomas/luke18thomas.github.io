2c66caf9e55641c949fd0e10b928435bc8f2a9e8
luke18thomas <luke18thomas@gmail.com>
2022-12-13 00:08:56 -0500

    Push

--- code.js
+++ code.js
@@ -8,13 +8,17 @@ document.addEventListener('keypress', function(e) {
 document.getElementById('photo').addEventListener("click", function(e) {
 
     var x = e.clientX;
-    let mid = document.getElementById('photo').width
+    let mid = document.getElementById('photo').width;
     
     if (x < mid/2) changePhoto('left')
     else changePhoto('right')
 })
 
-/*document.getElementByClassName('h1')*/
+function setSelection(s) {
+    selection = s;
+    console.log(s);
+    console.log(selection);
+}
 
 function changePhoto(dir) {
 
@@ -28,12 +32,16 @@ function changePhoto(dir) {
             i = 0
         else i++
     }
-
-    document.getElementById('photo').src = files[i]
+    console.log(i);
+    document.getElementById('photo').src = files[i];
 }
+
+var selection = 'selection';
+
+document.getElementById('category').innerHTML = '<h1 id="category">weehawken</h1>'
 
 var i = 0;
 
 var files = [];
 
-files.push("https://luke18thomas.github.io/1_000213600007.jpg")
+files.push("https://luke18thomas.github.io/photos/1_000213600007.jpg")
