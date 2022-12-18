23838f0af27dbc881c51c900b6700f4082f0bb46
luke18thomas <luke18thomas@gmail.com>
2022-12-18 16:56:55 -0500

    blah

--- code.js
+++ code.js
@@ -4,15 +4,17 @@ var pick = "";
 
 var files = [];
 
-files.push("photos/weehawken/weehawken-01.jpg");
-files.push("photos/weehawken/weehawken-02.jpg");
-files.push("photos/weehawken/weehawken-03.jpg");
-files.push("photos/weehawken/weehawken-04.jpg");
-files.push("photos/weehawken/weehawken-05.jpg");
-files.push("photos/weehawken/weehawken-06.jpg");
-files.push("photos/weehawken/weehawken-07.jpg");
-files.push("photos/weehawken/weehawken-08.jpg");
-files.push("photos/weehawken/weehawken-09.jpg");
+files.push("photos/film/film-01.jpg");
+files.push("photos/film/film-02.jpg");
+files.push("photos/film/film-03.jpg");
+files.push("photos/film/film-04.jpg");
+files.push("photos/film/film-05.jpg");
+files.push("photos/film/film-06.jpg");
+files.push("photos/film/film-07.jpg");
+files.push("photos/film/film-08.jpg");
+files.push("photos/film/film-09.jpg");
+files.push("photos/film/film-10.jpg");
+files.push("photos/film/film-11.jpg");
 
 
 function changePage(p) {
