f9b940b0d91e1d07b29793ba23bd66f4167892fb
luke18thomas <luke18thomas@gmail.com>
2023-09-09 01:55:37 -0400

    blah

--- code.js
+++ code.js
@@ -36,7 +36,7 @@ if ((current_hour >= 19 && current_hour <= 23) || (current_hour <= 7)) {
     toggleDarkMode();
 }
 
-/*loadPhotos("summer")
+/*loadPhotos("film")
 document.querySelector("#photos").style.display = "flex";
 document.querySelector("#thumbs").style.display = "flex";*/
 
